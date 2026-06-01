6bb31334b8a3f65899c9444c67aada77f28b3872
Eduardo Marin Espinoza <emarine@Eduardos-Laptop.local>
2026-06-01 16:55:39 -0400

    fix: configure selenium and chrome for mac arm64 environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,40 +1,49 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
-begin
-  require 'webdrivers'
-rescue LoadError
-  # webdrivers gem is optional; make sure chromedriver is available in PATH or with CHROMEDRIVER_PATH
-end
 
 Capybara.register_driver :selenium_chrome_stable do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.binary = ENV['CHROME_BINARY_PATH'] || 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
+  # Ruta de Chrome en macOS
+  options.binary = ENV['CHROME_BINARY_PATH'] || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+  # Configuración general
+  options.add_argument('--window-size=1920,1080')
   options.add_argument('--disable-gpu')
-  options.add_argument('--disable-software-rasterizer')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--no-sandbox')
-  options.add_argument('--window-size=1920,1080')
 
-  service = Selenium::WebDriver::Service.chrome
-  service.path = ENV['CHROMEDRIVER_PATH'] if ENV['CHROMEDRIVER_PATH']
+  # Desactivar popups molestos de Chrome
+  options.add_argument('--disable-save-password-bubble')
+  options.add_argument('--disable-password-generation')
+  options.add_argument('--disable-notifications')
+
+  # Desactivar detección de contraseñas filtradas
+  options.add_argument('--disable-features=PasswordLeakDetection,PasswordManagerOnboarding,AutofillServerCommunication')
+
+  # Desactivar Password Manager
+  options.add_preference('credentials_enable_service', false)
+  options.add_preference('profile.password_manager_enabled', false)
+  options.add_preference('profile.password_manager_leak_detection', false)
+
+  # Opcional: ejecutar headless
+  # options.add_argument('--headless=new')
 
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    options: options,
-    service: service
+    options: options
   )
 end
 
 Capybara.default_driver = :selenium_chrome_stable
 Capybara.javascript_driver = :selenium_chrome_stable
+
+# Tiempo máximo de espera para elementos dinámicos
 Capybara.default_max_wait_time = 10
 
+# Hooks de limpieza
 After do
-  begin
-    Capybara.current_session.driver.quit
-  rescue Selenium::WebDriver::Error::InvalidSessionIdError
-  rescue StandardError
-  end
+  Capybara.current_session.driver.quit
+rescue StandardError
 end
